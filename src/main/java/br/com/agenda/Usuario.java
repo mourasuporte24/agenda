b13890cdbd442528fa8bbfec68b6e20c0ffdb908
Adenilson Pantoja Moura <69207@PROWAY.TREINA>
2020-11-01 15:28:04 -0300

    Mvc -Model

--- src/main/java/br/com/agenda/Usuario.java
+++ src/main/java/br/com/agenda/Usuario.java
@@ -2,4 +2,24 @@ package br.com.agenda;
 
 public class Usuario {
 
+	private Long id;
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
+
